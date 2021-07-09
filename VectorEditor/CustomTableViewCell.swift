65beef78f580c3cd074a810568bddf3bdc9daede
Oleksii <al.bashuk@gmail.com>
2021-07-09 11:49:34 +0300

    list added

---
+++ VectorEditor/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  VectorEditor
+//
+//  Created by Oleksii Bashuk on 07.07.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var animalLbl: UILabel!
+    @IBOutlet weak var cellView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- VectorEditor/ViewController.swift
+++ VectorEditor/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource  {
 
+    let elements = ["cat", "dog", "frog"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        tableView.dataSource = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        
+        cell.animalLbl.text = elements[indexPath.row]
+        
+        return cell
+    }
 }
 
